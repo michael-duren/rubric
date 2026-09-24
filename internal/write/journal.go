870044ee401006d3866f1ed52e4d24ba241d7f9a
Michael Duren <michaeld@michaelduren.com>
2026-09-24 08:34:10 -0500

    feat: apply initialization plans with recovery

---
+++ internal/write/journal.go
@@ -0,0 +1,25 @@
+package write
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"io/fs"
+)
+
+type entry struct {
+	path    string
+	existed bool
+	old     []byte
+	oldMode fs.FileMode
+	written string
+}
+
+type journal struct {
+	entries []entry
+	dirs    []string
+}
+
+func digest(data []byte) string {
+	sum := sha256.Sum256(data)
+	return hex.EncodeToString(sum[:])
+}
